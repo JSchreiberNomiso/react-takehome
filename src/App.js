bf9f857bd2c576fd1323f07fec0943926b569dd8
Justin Schreiber <justin.schreiber@dish.com>
2022-01-17 16:41:37 -0700

    added styling and basic setup

--- src/App.js
+++ src/App.js
@@ -3,17 +3,39 @@ import { getData } from "./helpers/getData";
 import "./styles.css";
 
 export default function App() {
-  useEffect(() => {
-    getData()
-      .then(() => null)
-      .catch((error) => console.log(error));
-  });
-
   return (
     <div className="App">
-      <div>
-        <h1>Hello CodeSandbox</h1>
-        <h2>Start editing to see some magic happen!</h2>
+      <h1>Gallery Builder</h1>
+      <form>
+        <span>Title:</span>
+        <input type="text"/>
+        <span>Description:</span>
+        <input type="text"/>
+      </form>
+      <button className="">New</button>
+      <div className="widget">
+        <div className="itemContainer">
+          <div className="textContainer">
+            <h2>Title</h2>
+            <h3>Description</h3>
+          </div>
+          <div className="imgContainer">
+            <img src="https://loremflickr.com/400/225/code" />
+          </div>
+        </div>
+        <button className="delete">Delete</button>
+      </div>
+      <div className="widget">
+        <div className="itemContainer">
+          <div className="textContainer">
+          <h2>Title</h2>
+            <h3>Description</h3>
+          </div>
+          <div className="imgContainer">
+            <img src="https://loremflickr.com/400/225/code" />
+          </div>
+        </div>
+        <button className="delete">Delete</button>
       </div>
     </div>
   );
