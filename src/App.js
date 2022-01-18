611d3e48aa1899a21faea2d68f80cac4de87ba08
Justin Schreiber <justin.schreiber@dish.com>
2022-01-18 09:02:17 -0700

    minor styling changes

--- src/App.js
+++ src/App.js
@@ -17,10 +17,10 @@ export default function App() {
         <div className="itemContainer">
           <div className="textContainer">
             <h2>Title</h2>
-            <h3>Description</h3>
+            <h3>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</h3>
           </div>
           <div className="imgContainer">
-            <img src="https://loremflickr.com/400/225/code" />
+            <img src="https://loremflickr.com/225/400/code" />
           </div>
         </div>
         <button className="delete">Delete</button>
